fa28555bd0016d11141d0b315b68a8d7ca026e14
hoanhvuu <hoanhvu1501@gmail.com>
2026-06-16 18:04:38 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Hồ Anh Vũ đã load xong!');
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
